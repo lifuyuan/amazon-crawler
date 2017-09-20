e4505045b62c2c6f9c7c92701369e8e2da842c8f
fuyuan <lifuyuan33@gmail.com>
2017-09-20 23:28:57 +0800

    twisted added & extractors new

--- extractors.py
+++ extractors.py
@@ -1,4 +1,8 @@
 from html.parser import HTMLParser
+import html
+from helpers import format_url
+import requests
+import os
 
 htmlparser = HTMLParser()
 
@@ -12,3 +16,86 @@ def get_category(page):
         category = "{}{}".format(category, last_tag.string)
 
     return category
+
+
+def get_title(item):
+    try:
+        title_tag = item.select("h2.s-access-title")[0]
+        if title_tag:
+            return html.unescape(title_tag.string)
+        else:
+            return "<missing product title>"
+    except IndexError as err:
+        return "<missing product title>"
+    except Exception as e:
+        return "<missing product title>"
+
+
+def get_url(item):
+    try:
+        link_tag = item.select("a.s-access-detail-page")[0]
+        if link_tag:
+            return format_url(link_tag['href'])
+        else:
+            return "<missing product url>"
+    except IndexError as err:
+        return "<missing product url>"
+    except Exception as e:
+        return "<missing product url>"
+
+
+def get_asin(item):
+    asin = item['data-asin']
+    if asin:
+        return asin
+    else:
+        return "<missing product asin>"
+
+
+def get_price(item):
+    try:
+        price_tag = item.select(".sx-price")[0]
+        if price_tag:
+            currency = ""
+            whole = ""
+            fractional = ""
+            currency_tag = price_tag.select(".sx-price-currency")[0]
+            if currency_tag:
+                currency = currency_tag.string
+            whole_tag = price_tag.select(".sx-price-whole")[0]
+            if whole_tag:
+                whole = whole_tag.string
+            fractional_tag = price_tag.select(".sx-price-fractional")[0]
+            if fractional_tag:
+                fractional = fractional_tag.string
+            return "{}{}.{}".format(currency, whole, fractional)
+        else:
+            return "<missing product price>"
+    except IndexError as err:
+        return "<missing product price>"
+    except Exception as e:
+        return "<missing product price>"
+
+
+def get_primary_img(item):
+    try:
+        asin = get_asin(item)
+        thumb_tag = item.select(".s-access-image")[0]
+        if thumb_tag:
+            return thumb_tag['src']
+        else:
+            return "<missing product img>"
+    except IndexError as err:
+        return "<missing product img>"
+    except Exception as e:
+        return "<missing product img>"
+
+
+def download_img(url, category, asin):
+    dir_name = "images/{}".format(category)
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+    path = "{}/{}.jpg".format(dir_name, asin)
+    with open(path, "wb") as f:
+        f.write(requests.get(url).content)
+    return path
--- settings.py
+++ settings.py
@@ -12,6 +12,14 @@ redis_host = "127.0.0.1"
 redis_port = 6379
 redis_db = 0
 
+# Database
+pgs_database = "amazon_crawler"
+pgs_host = "127.0.0.1"
+pgs_user = "amazon_info"
+pgs_password = "1qaz2wsx"
+pgs_port = "5432"
+
+
 # requests
 headers = {
     # "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",

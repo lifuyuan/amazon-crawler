4172f83cb8b92566abc8bfa9076db4f7f989e3f5
fuyuan <lifuyuan33@gmail.com>
2017-09-20 16:57:13 +0800

    amazon crawler init

---
+++ crawler.py
@@ -0,0 +1,46 @@
+import helpers
+import eventlet
+from datetime import datetime
+import settings
+
+crawl_time = datetime.now()
+
+pool = eventlet.GreenPool(settings.max_threads)
+pile = eventlet.GreenPile(pool)
+
+
+def begin_crawl_categories():
+    start_url = "https://www.amazon.com/gp/site-directory/ref=nav_shopall_btn"
+    page, html = helpers.make_request(start_url)
+    if not page:
+        return
+    for tag in page.select('.fsdDeptCol a'):
+        helpers.enqueue_categories_url(tag['href'])
+
+
+def crawl_categories():
+    url = helpers.dequeue_categories_url()
+    if not url:
+        helpers.log("WARNING: No URLs found in the queue. Retrying...")
+        pile.spawn(crawl_categories)
+        return
+    page, html = helpers.make_request(url.decode())
+    if not page:
+        return
+    items = page.select('#search-results .s-result-item')
+    if len(items) > 0:
+        helpers.enqueue_items_url(url.decode())
+    subcategories = page.select('.s-ref-indent-one li a')
+    subcategories.extend(page.select('.s-ref-indent-two li a'))
+    subcategories.extend(page.select('.a-carousel li  .list-item__category-link'))
+    for tag in subcategories:
+        helpers.enqueue_categories_url(tag['href'])
+    pile.spawn(crawl_categories)
+
+
+if __name__ == "__main__":
+    begin_crawl_categories()
+    helpers.log("Beginning crawl at {}".format(crawl_time))
+    [pile.spawn(crawl_categories) for _ in range(settings.max_threads)]
+    pool.waitall()
+
---
+++ helpers.py
@@ -0,0 +1,91 @@
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+from datetime import datetime
+import settings
+import random
+import requests
+import os
+import redis
+from requests.exceptions import RequestException
+num_requests = 0
+
+redis = redis.StrictRedis(host=settings.redis_host, port=settings.redis_port, db=settings.redis_db)
+
+
+def make_request(url, return_soup=True):
+    # 全局的url request及response处理函数
+    url = format_url(url)
+    log("make_request")
+    log(url)
+    if "picassoRedirect" in url:
+        return None, None  # 跳过重定向的url
+
+    global num_requests
+    if num_requests >= settings.max_requests:
+        raise Exception("Reached the max number of requests: {}".format(settings.max_requests))
+
+    headers = settings.headers
+    headers["User-Agent"] = random.choice(settings.agents)
+    # proxy_dict = {
+    #     "https": random.choice(settings.proxies)
+    # }
+    try:
+        r = requests.get(url, headers=headers)
+    except RequestException as e:
+        log("WARNING: Request for {} failed.".format(url))
+        return make_request(url)  # try request again, recursively
+
+    num_requests += 1
+    if r.status_code != 200:
+        os.system('say "Got non-200 Response"')
+        log("WARNING: Got a {} status code for URL: {}".format(r.status_code, url))
+        return None, None
+
+    if return_soup:
+        return BeautifulSoup(r.text, "lxml"), r.text
+    return r
+
+
+def format_url(url):
+    u = urlparse(url)
+    scheme = u.scheme or 'https'
+    host = u.netloc or 'www.amazon.com'
+    path = u.path
+
+    if not u.query:
+        query = ""
+    else:
+        query = "?"
+        for piece in u.query.split('&'):
+            k, v = piece.split("=")
+            query += "{k}={v}&".format(**locals())
+        query = query[:-1]
+
+    return "{scheme}://{host}{path}{query}".format(**locals())
+
+
+def log(msg):
+    if settings.log_stdout:
+        try:
+            print("{}:{}".format(datetime.now(), msg))
+        except UnicodeEncodeError:
+            pass
+
+
+def enqueue_categories_url(url):
+    url = format_url(url)
+    return redis.sadd("categories_queue", url)
+
+
+def dequeue_categories_url():
+    return redis.spop("categories_queue")
+
+
+def enqueue_items_url(url):
+    url = format_url(url)
+    return redis.sadd("items_queue", url)
+
+
+def dequeue_items_url():
+    return redis.spop("items_queue")
+
